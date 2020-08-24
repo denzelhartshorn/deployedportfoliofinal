562387501f563cdd08d89bd64a8250248630692d
Denzel Hartshorn <denzelhartshorn@gmail.com>
2020-08-24 15:15:45 -0700

    first commit

---
+++ precache-manifest.7cb5005ab0633984c3a44ad5d6035516.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff51cd25254dc027e4a6",
+    "url": "/static/css/main.ac210ef9.chunk.css"
+  },
+  {
+    "revision": "ff51cd25254dc027e4a6",
+    "url": "/static/js/main.ff51cd25.chunk.js"
+  },
+  {
+    "revision": "6221052083d9d7c06dcf",
+    "url": "/static/js/1.62210520.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f41a915bc40dbed62d77e76073840077",
+    "url": "/index.html"
+  }
+];
